9a46f5b8fd3f15e2aa07b66616bcf958aa96171c
lahiru <lahiru@lahirus-mbp>
2017-03-26 14:58:16 +0530

    Adding line and column identifications for method, variable and parameters.

--- src/lk/devfactory/scitool/Apitest.java
+++ src/lk/devfactory/scitool/Apitest.java
@@ -11,11 +11,46 @@ public class Apitest {
 
 	public static void main(String[] args) {
 		System.out.println("###############");
+		lineAndColomNumbersMethodsVariables();
+		System.out.println("***************");
 		unusedPrivateFunctions();
 		System.out.println("@@@@@@@@@@@@@@@");
 		unusedPrivateVariables();
 		System.out.println("&&&&&&&&&&&&&&&");
 		unusedParameters();
+	}
+	
+	private static void lineAndColomNumbersMethodsVariables() {
+		try {
+			// Open the Understand Database
+			Database db = Understand.open(projPath);
+			// Get a list of all functions and methods
+			Entity[] clazz = db.ents("class ~unknown ~unresolved");
+			for (Entity e : clazz) {
+				System.out.println(e.simplename()+" : "+e.kind());
+				
+				// Get a list of all variables of the class
+				Reference[] varRefs = e.refs("define","variable",true);
+				for (Reference vRef : varRefs) {
+					System.out.println(e.name() + " => " + vRef.ent().name() +" ["+vRef.line()+":"+vRef.column()+"] > "+vRef.ent().kind().name());
+				}
+				
+				// Get a list of all functions and methods of class
+				Reference[] methodRefs = e.refs("define","method",true);
+				for (Reference mRef : methodRefs) {
+					System.out.println(e.name() + " => " + mRef.ent().name() +" ["+mRef.line()+":"+mRef.column()+"] > "+mRef.ent().kind().name());
+					
+					// Get a list of all method variables
+					Reference[] localVarRefs = mRef.ent().refs("define", "Variable Parameter", true);
+					for (Reference lVRef : localVarRefs) {
+						System.out.println(mRef.ent().name() + " =>* " + lVRef.ent().name() +" ["+lVRef.line()+":"+lVRef.column()+"] > "+lVRef.ent().kind().name());
+					}
+				}
+			}
+		} catch (UnderstandException e) {
+			System.out.println("Failed opening Database:" + e.getMessage());
+			System.exit(0);
+		}
 	}
 
 	private static void unusedPrivateFunctions() {
@@ -23,12 +58,12 @@ public class Apitest {
 			// Open the Understand Database
 			Database db = Understand.open(projPath);
 			// Get a list of all functions and methods
-			Entity[] funcs = db.ents("function ~unknown ~unresolved,method ~unknown ~unresolved");
+			Entity[] funcs = db.ents("function private ~unknown ~unresolved,method private ~unknown ~unresolved");
 			for (Entity e : funcs) {
 				System.out.println(e.name()+" : "+e.kind());
 				
 				// Get a list of all functions and methods has invocations
-				Reference[] callRefs = e.refs("call",null,true);
+				Reference[] callRefs = e.refs("callby",null,true);
 				for (Reference pRef : callRefs) {
 					System.out.println(e.name() + " => " + pRef.ent().name() +" ["+pRef.line()+":"+pRef.column()+"]");
 				}
@@ -75,7 +110,7 @@ public class Apitest {
 				Reference[] paramterRefs = e.refs("define", "parameter", true);
 				for (Reference pRef : paramterRefs) {
 					e = pRef.ent();
-					System.out.println(e.name() + " => " + pRef.ent().name() +" ["+pRef.line()+":"+pRef.column()+"]");
+					System.out.println("Param => " + e.name());
 					
 					// Get a list of all used params
 					Reference[] useByRefs = e.refs("Useby",null,true);
